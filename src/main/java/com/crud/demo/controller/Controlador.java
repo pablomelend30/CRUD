133c20fd20bb7a768e00e9d8fbeacc97ddb16b2e
pablomelend30 <pmelendezm@miumg.edu.gt>
2022-06-03 08:16:14 -0600

    make it better

--- src/main/java/com/crud/demo/controller/Controlador.java
+++ src/main/java/com/crud/demo/controller/Controlador.java
@@ -1,5 +1,7 @@
 package com.crud.demo.controller;
 
+import com.crud.demo.dto.actualizarRegistroDto;
+import com.crud.demo.dto.eliminarRegistroDto;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,44 +16,69 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.crud.demo.interfaceService.IpersonaService;
 import com.crud.demo.modelo.Persona;
+import java.util.Date;
 import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
 public class Controlador {
 
-	@Autowired
-	private IpersonaService service;
+    @Autowired
+    private IpersonaService service;
 
-	@GetMapping("/listar")
-	public String listar(Model model) {
-		List<Persona> personas = service.listar();
-		model.addAttribute("personas", personas);
-		return "index";
-	}
+    @GetMapping("/listar")
+    public String listar(Model model) {
+        List<Persona> personas = service.obtenerRegistros();
+        model.addAttribute("personas", personas);
+        return "index";
+    }
 
-	@GetMapping("/new")
-	public String agregar(Model model) {
-		model.addAttribute("persona", new Persona());
-		return "form";
-	}
+    @GetMapping("/new")
+    public String agregar(Model model) {
+        model.addAttribute("persona", new Persona());
+        return "form";
+    }
 
-	@PostMapping("/save")
-	public String save(@Valid Persona p, Model model) {
-		service.save(p);
-		return "redirect:/listar";
-	}
+    @PostMapping("/save")
+    public String save(@Valid Persona p, Model model) {
+        service.save(p);
+        return "redirect:/listar";
+    }
 
-	@GetMapping("/editar/{id}")
-	public String editar(@PathVariable int id, Model model) {
-		Optional<Persona> persona = service.listarId(id);
-		model.addAttribute("persona", persona);
-		return "form";
-	}
+    @PostMapping("/update")
+    public String update(actualizarRegistroDto dto) {
 
-	@GetMapping("/eliminar/{id}")
-	public String delete(Model model, @PathVariable int id) {
-		service.delete(id);
-		return "redirect:/listar";
-	}
+        final Date now = new Date();
+
+        dto.setFechaModificacion(now);
+
+        service.actualizar(dto);
+        return "redirect:/listar";
+    }
+
+    @PostMapping("/eliminar")
+    public String eliminarRegistro(eliminarRegistroDto dto) {
+
+        service.eliminar(dto);
+        return "redirect:/listar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable int id, Model model, Persona p) {
+        Optional<Persona> persona = service.listarId(id);
+        model.addAttribute("persona", persona);
+
+        return "formUpdate";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable int id, Model model, Persona p) {
+        Optional<Persona> persona = service.listarId(id);
+        model.addAttribute("persona", persona);
+
+        return "formDelete";
+    }
+
 }
