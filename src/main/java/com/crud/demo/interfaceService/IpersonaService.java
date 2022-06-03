cac580e57ba6b7a239ba092a6dda2c1d4f26c2db
pablomelend30 <pmelendezm@miumg.edu.gt>
2022-06-03 08:10:02 -0600

    Se modifican metodos

--- src/main/java/com/crud/demo/interfaceService/IpersonaService.java
+++ src/main/java/com/crud/demo/interfaceService/IpersonaService.java
@@ -1,5 +1,7 @@
 package com.crud.demo.interfaceService;
 
+import com.crud.demo.dto.actualizarRegistroDto;
+import com.crud.demo.dto.eliminarRegistroDto;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,8 +11,11 @@ public interface IpersonaService {
 	
 	public List<Persona>listar();
 	public Optional<Persona>listarId(int id);
-	public int save(Persona p);
+	public Persona save(Persona p);
+        public Persona update(Persona p);
 	public void delete(int id);
-	
+	public Persona actualizar(actualizarRegistroDto dto);
+        public Persona eliminar(eliminarRegistroDto dto);
+        public List<Persona> obtenerRegistros();
 
 }
--- src/main/java/com/crud/demo/interfaces/IPersona.java
+++ src/main/java/com/crud/demo/interfaces/IPersona.java
@@ -4,8 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.crud.demo.modelo.Persona;
+import java.util.List;
 
 @Repository
 public interface IPersona extends CrudRepository<Persona, Integer>{
-
+    @Override
+    List<Persona> findAll();
 }
